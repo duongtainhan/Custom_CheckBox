ac7734e36ca6e8ea16112d5be630c86faf8f4dba
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-15 22:22:19 +0700

    set on checked listener

--- app/src/main/java/com/example/duongtainhan555/custom_checkbox/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/custom_checkbox/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.duongtainhan555.custom_checkbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private CheckBox checkBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Anh xa
+        checkBox = findViewById(R.id.customCheckbox);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(checkBox.isChecked())
+                {
+                    checkBox.setText("   ON");
+                }
+                else
+                {
+                    checkBox.setText("   OFF");
+                }
+            }
+        });
     }
 }
